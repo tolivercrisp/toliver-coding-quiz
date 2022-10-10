3609bf3ed48469f1db6dfcdf28bebc4189ffe2ba
Toliver Crisp <toliverjcrisp@gmail.com>
2022-10-10 17:49:52 -0400

    Created question array with answer array in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,48 @@
 var questions = [
     {
-        prompt: "Commonly used data types DO NOT include:"
+    question: 'Commonly used data types DO NOT include:',
+    answers: [
+      { text: '1. strings', correct: false },
+      { text: '2. booleans', correct: false },
+      { text: '3. alerts', correct: true },
+      { text: '4. numbers', correct: false }
+    ]
+    },
+    {
+    question: 'The condition in an if / else statement is enclosed within _____.',
+    answers: [
+        { text: '1. quotes', correct: false },
+        { text: '2. curly brackets', correct: false },
+        { text: '3. parenthesis', correct: false },
+        { text: '4. square brackets', correct: true }
+    ]
+    },
+    {
+    question: 'Arrays in Javascript can be used to store _____.',
+    answers: [
+        { text: '1. numbers and strings', correct: false },
+        { text: '2. other arrays', correct: false },
+        { text: '3. booleans', correct: false },
+        { text: '4. all of the above', correct: true }
+    ]
+    },
+    {
+    question: 'String values must be enclosed within _____ when being assigned to variables.',
+    answers: [
+        { text: '1. commas', correct: false },
+        { text: '2. curly brackets', correct: false },
+        { text: '3. quotes', correct: false },
+        { text: '4. parenthesis', correct: true }
+    ]
+    },
+    {
+    question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answers: [
+        { text: '1. Javascript', correct: false },
+        { text: '2. terminal / bash', correct: false },
+        { text: '3. for loops', correct: false },
+        { text: '4. console.log', correct: true }
+    ]
     }
 
 ]
